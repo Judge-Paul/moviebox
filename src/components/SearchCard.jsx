2ba70ca1ef4f2e91df87464710ee2214542ca29f
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-09-16 11:00:18 +0100

    Update SearchCard.jsx

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -6,7 +6,7 @@ import reactLogo from "../assets/react.svg";
 
 export default function SearchCard({ id, title, poster_path, overview }) {
   return (
-    <Link to={`/movies/${id}`} className="w-full">
+    <Link to={`/movies/${id}`} className="w-[23rem]">
       <div className="flex mb-3 hover:bg-blue-400">
         <img
           src={`http://images.tmdb.org/t/p/w92/original${poster_path}`}
