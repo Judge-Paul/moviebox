cc06c0e65c0808bcb147bb6a9d4e6e4864377db2
Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
2023-09-16 14:22:26 +0100

    feat: add liking feature

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FiHeart } from "react-icons/fi";
 import tomato from "../assets/tomato.png";
+import { toast } from 'sonner';
 
 export default function MovieCard({
   id,
@@ -10,17 +12,36 @@ export default function MovieCard({
   vote_average,
   genre_ids,
 }) {
-  const [isLike, setIsLiked] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLikeClick = () => {
+    if (isLiked) {
+      setIsLiked(false);
+      toast(`"${title}" has been removed from favorites`);
+    } else {
+      setIsLiked(true);
+      toast(`"${title}" has been added to favorites`);
+    }
+  };
+
   return (
-    <Link to={`/movies/${id}`}  className="mt-10 hover:bg-gray-50 mx-3" data-testid="movie-card">
-      <img
-        src={`http://images.tmdb.org/t/p/original${poster_path}`}
-        alt={title}
-        className="w-full h-[22rem] bg-gray-200 flex justify-center items-center"
-data-testid="movie-poster" 
-      />
+    <Link to={`/movies/${id}`} className="mt-10 hover:bg-gray-50 mx-3" data-testid="movie-card">
+      <div className="relative">
+        <button
+          onClick={handleLikeClick}
+          className={`absolute top-0 right-0 m-2 bg-transparent border-none outline-none cursor-pointer ${isLiked ? 'text-pink-500' : 'text-gray-500'}`}
+        >
+          <FiHeart size={24} />
+        </button>
+        <img
+          src={`http://images.tmdb.org/t/p/original${poster_path}`}
+          alt={title}
+          className="w-full h-[22rem] bg-gray-200 flex justify-center items-center"
+          data-testid="movie-poster"
+        />
+      </div>
       <div className="mt-3">
-        <p className="text-gray-500 text-sm" data-testid="movie-release-date" >{release_date}</p>
+        <p className="text-gray-500 text-sm" data-testid="movie-release-date">{release_date}</p>
         <h4 className="mt-2.5 font-semibold" data-testid="movie-title">{title}</h4>
         <div className="mt-2.5 flex justify-between text-sm font-medium">
           <div className="flex">
